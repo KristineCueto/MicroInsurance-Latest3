cefc41f728a152e3f3d04419b3b9c535737d65b8
Kristine <cueto.kristine@yahoo.com.ph>
2015-11-26 11:21:42 +0800

    Extending FT and creating a test fixture to load data into the database from text files

--- fts/tests.py
+++ fts/tests.py
@@ -1,7 +1,9 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class MicroTest(LiveServerTestCase):
+	fixtures = ['admin_user.json']
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -11,12 +13,29 @@ class MicroTest(LiveServerTestCase):
 		self.browser.quit()
 
 	def test_can_create_maintenance_via_admin_site(self):
-		#opens the web browser, and goes to the admin page
+		#Kristine opens the web browser, and goes to the admin page
 		self.browser.get(self.live_server_url + '/admin/')
 
-		#Sees the 'Django administration' heading
+		#She sees the 'Django administration' heading
 		body = self.browser.find_element_by_tag_name('body')
 		self.assertIn('Django administration', body.text)
+
+		#She types in her username and passwords and hits return
+		username_field = self.browser.find_element_by_name('username')
+		username_field.send_keys('admin')
+
+		password_field = self.browser.find_element_by_name('password')
+		password_field.send_keys('admin')
+		password_field.send_keys(Keys.RETURN)
+
+		#her username and password are accepted, and she is taken to
+		#the Site Administration page
+		body = self.browser.find_element_by_tag_name('body')
+		self.assertIn('Site administration', body.text)
+
+		#She now sees a couple of hyperlink that says "Branch"
+		branches_links = self.browser.find_elements_by_link_text('Branch')
+		self.assertEquals(len(branches_links), 2)
 
 		#TODO: use the admin site to create maintenance of the platform
 		self.fail('Finish the Test') 
